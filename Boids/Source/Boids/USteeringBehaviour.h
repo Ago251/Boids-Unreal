9086788d3179c8381291999245c33130ee7039fc
Andrea Agostini <andreaagostini1998@gmail.com>
2024-10-22 00:06:03 +0200

    Created USteeringBehaviour class

---
+++ Boids/Source/Boids/USteeringBehaviour.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "USteeringBehaviour.generated.h"
+
+/**
+ * 
+ */
+UCLASS(Abstract, Blueprintable)
+class BOIDS_API UUSteeringBehaviour : public UObject
+{
+	GENERATED_BODY()
+	
+};
