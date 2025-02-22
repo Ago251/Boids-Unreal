87037bcca09a3beee7cd26c4c88e383999707243
Andrea Agostini <andreaagostini1998@gmail.com>
2025-02-22 09:36:03 +0100

    Refactoring WanderBehaviour

--- Boids/Source/Boids/Private/WanderBehaviour.cpp
+++ Boids/Source/Boids/Private/WanderBehaviour.cpp
@@ -3,39 +3,23 @@
 
 #include "WanderBehaviour.h"
 
-UWanderBehaviour::UWanderBehaviour()
-{
-	WanderTarget = FVector(FMath::FRandRange(-1.f, 1.f), FMath::FRandRange(-1.f, 1.f), 0).GetSafeNormal() * WanderCircleRadius;
-}
-
 FVector UWanderBehaviour::CalculateSteeringForce_Implementation(AActor* Owner)
 {
 	 if (!Owner) return FVector::ZeroVector;
 	
     FVector BoidLocation = Owner->GetActorLocation();
     FVector Forward = Owner->GetActorForwardVector();
-	
-	float ActorRadius = 0.f;
-	if (Owner) {
-		FVector Origin, BoxExtent;
-		Owner->GetActorBounds(true, Origin, BoxExtent);
-		ActorRadius = BoxExtent.Size() * 0.2f;        
-	}
 
-	FVector WanderCircleCenter = BoidLocation + Forward * (WanderCircleRadius + ActorRadius);
-
+	FVector WanderCircleCenter = BoidLocation + Forward * WanderCircleDistance;
 	
-    WanderTarget += FVector(FMath::FRandRange(-WanderJitter, WanderJitter), FMath::FRandRange(-WanderJitter, WanderJitter), FMath::FRandRange(-WanderJitter, WanderJitter));
+    WanderTarget += FVector(FMath::FRandRange(-WanderJitter, WanderJitter),
+    						FMath::FRandRange(-WanderJitter, WanderJitter),
+    						FMath::FRandRange(-WanderJitter, WanderJitter));
 	
     WanderTarget = WanderTarget.GetClampedToSize(-WanderCircleRadius, WanderCircleRadius);
 	
     FVector WanderTargetWorld = WanderCircleCenter + WanderTarget;
-	
     FVector WanderForce = WanderTargetWorld - BoidLocation;
 	
-    DrawDebugSphere(GetWorld(), WanderCircleCenter, 10.f, 12, FColor::Blue, false, 0.0f);
-    DrawDebugSphere(GetWorld(), WanderTargetWorld, 10.f, 12, FColor::Red, false, 0.0f);
-    DrawDebugLine(GetWorld(), BoidLocation, WanderTargetWorld, FColor::Green, false, 0.0f, 0, 1.f);
-	
-    return (WanderForce.GetSafeNormal() * WanderCircleRadius) * Strength;
+    return WanderForce.GetSafeNormal() * Strength;
 }
